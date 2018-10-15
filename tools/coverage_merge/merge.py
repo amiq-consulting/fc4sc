d2b26baa6ce2c447d6cfbc72f15676719aa5f2d7
Dragos Dospinescu <dragos.dospinescu@amiq.com>
2018-10-15 12:01:43 +0300

    Implemented more parser functionality for merge.py: Now traverses all covergroups/coverpoints/bins and reads number of hits

--- tools/coverage_merge/merge.py
+++ tools/coverage_merge/merge.py
@@ -51,10 +51,33 @@ def parseXML(name):
         else:
             print (child.tag)
     
-    # TODO: parse covergroups?
-    for instanceCoverages in root.findall('{UCIS}instanceCoverages'):
-        moduleName = instanceCoverages.attrib['moduleName']
-        print(moduleName)
+    
+    # namespace map
+    nsstr = 'ucis'
+    ns = {nsstr : 'UCIS'}
+    
+    # parse module hierarchy (instanceCoverages elements)
+    for instanceCoverages in root.findall('{0}:instanceCoverages'.format(nsstr), ns):
+        print("Covergroup type: {0}; {1}".format(instanceCoverages.get('moduleName'), instanceCoverages.attrib))
+        # parse covergroups (covergroupCoverage elements)
+        for covergroupCoverage in instanceCoverages.findall('{0}:covergroupCoverage'.format(nsstr), ns):
+            # parse covergroup instances (cgInstance elements)
+            for cgInstance in covergroupCoverage.findall('{0}:cgInstance'.format(nsstr), ns):
+                print ("\t[cgInstance] {0}".format(cgInstance.get('name')))
+                # parse coverpoints
+                for coverpoint in cgInstance.findall('{0}:coverpoint'.format(nsstr), ns):
+                    print ("\t\t[coverpoint] {0}".format(coverpoint.get('name')))
+                    # parse bins
+                    for bin in coverpoint.findall('{0}:coverpointBin'.format(nsstr), ns):
+                        totalhits = 0
+                        # parse all bin ranges
+                        for range in bin.findall('{0}:range'.format(nsstr), ns):
+                            contents = range.find('{0}:contents'.format(nsstr), ns)
+                            rangeHitCount = int(contents.get('coverageCount'))
+                            totalhits = totalhits + rangeHitCount
+                            
+                        print ("\t\t\t[bin:{1}] {0} -> {2}".format(
+                            bin.get('name'), bin.get('type'), totalhits))
 
 if __name__ == "__main__":
     # the search top directory is by default the execution directory 
@@ -71,6 +94,9 @@ if __name__ == "__main__":
     for filename in find_xmls(search_top_dir):
         print ('*****Parsing XML: ' + filename)
         parseXML(filename)
-               
+    
+    # TODO: parse the resulted DB and change the "UCIS ID" attributes to be unique
+    
+    
 
     print("Done!")
