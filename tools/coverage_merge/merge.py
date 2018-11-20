a613ae99c0878995ed7a0d327495c444ad4a292b
Dragos Dospinescu <dragos.dospinescu@amiq.com>
2018-11-20 12:02:08 +0200

    Revert "merge.py: Fixed "parse_covergroup_type" problem added in pull request 8" This reverts commit 96410f1f467f282a3d1a4c4ea411565a091c92b2. It turns out the problem lays somewhere else (issue still not fixed)!

--- tools/coverage_merge/merge.py
+++ tools/coverage_merge/merge.py
@@ -243,7 +243,8 @@ class UCIS_DB_Parser:
 
     def parse_covergroup_type(self, covergroup_type, parent_query):
         """ Parse covergroup instance """
-        for cgInstance in self.findall_ucis_children(covergroup_type, "cgInstance"):
+        covergroupCoverage = self.find_ucis_element(covergroup_type,"covergroupCoverage")
+        for cgInstance in self.findall_ucis_children(covergroupCoverage, "cgInstance"):
             cgInstNameAttrib = 'name'
             cgInstName = cgInstance.get(cgInstNameAttrib)
             xpath_query = parent_query + "/" + "*[@{0}='{1}']".format(cgInstNameAttrib, cgInstName)
