94ae5262eef3f84b271a4053b806dac1581b82b4
Dragos Dospinescu <dragos.dospinescu@amiq.com>
2019-04-25 18:25:31 +0300

    Fixed javascript app buttons not working

--- tools/gui/fc4sc/js/cg_type.js
+++ tools/gui/fc4sc/js/cg_type.js
@@ -23,41 +23,37 @@
 ******************************************************************************/
 
 function CgType(node, cvg_tree) {
+    if (typeof node === "string" || node instanceof String) this.name = node;
+    else this.name = node.getAttribute("moduleName");
 
-  if (typeof node === 'string' || node instanceof String)
-    this.name = node;
-  else
-    this.name = node.getAttribute('moduleName');
-
-  this.cg_insts = cvg_tree;
-  
+    this.cg_insts = cvg_tree;
 }
 
-CgType.prototype.stringify = function(elem) {
+CgType.prototype.stringify = function() {
+    var result = "";
 
-  var result = "";
+    result +=
+        '<div style="text-align: center;" id="' +
+        this.name +
+        '">' +
+        "<h1>" +
+        this.name +
+        "</h1>";
 
-  result += "<div style=\"text-align: center;\" id=\"" + this.name + "\">" 
-  +    "<h1>"
-  +      this.name
-  +    "</h1>";
+    // elem.innerHTML += result;
 
-  elem.innerHTML += result;
+    for (var k = 0; k < this.cg_insts.length; ++k) {
+        result += this.cg_insts[k].stringify();
+    }
 
-  for (var k=0; k < this.cg_insts.length; ++k ) {
-      elem.innerHTML += this.cg_insts[k].stringify();
-  }
+    // elem.innerHTML += "</div>";
+    result += "</div>";
 
-  elem.innerHTML += "</div>";
+    console.log(result);
 
-  return result;
-
+    return result;
 };
 
 CgType.prototype.toc_stringify = function() {
-  return "<li>" 
-        +  "<a href=\"#" + this.name + "\">"
-        +    this.name 
-        +  "</a></li>";  
-
-}
+    return "<li>" + '<a href="#' + this.name + '">' + this.name + "</a></li>";
+};
