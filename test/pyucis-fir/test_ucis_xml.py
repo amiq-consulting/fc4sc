32f39e43271e028a49b8158569343fde14d2fc85
John F. Davis <davjohn@google.com>
2022-05-16 15:53:21 -0400

    wip

--- test/pyucis-fir/test_ucis_xml.py
+++ test/pyucis-fir/test_ucis_xml.py
@@ -6,7 +6,7 @@ from ucis.xml import validate_ucis_xml
 
 
 
-with open('fir.xml') as f:
+with open('../../examples/fir/coverage_results.xml') as f:
   xml_file = BytesIO(bytes(bytearray(f.read(), encoding='utf-8')))
   validate_ucis_xml(xml_file)
 
